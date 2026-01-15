f8276bd19ba7fb6fff575681767e06b5c924ae13
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-01-15 12:41:58 +0530

    Time: 0 ms (100%), Space: 14.8 MB (36.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>s;
         for(int i=0;i<nums.size();i++){
-            s[nums[i]]=i;
-        }
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
             if(s.contains(target-nums[i]) && s[target-nums[i]]!=i){
-                ans={i,s[target-nums[i]]};
+                vector<int>ans={i,s[target-nums[i]]};
                 return ans;
             }
+            s[nums[i]]=i;
         }
-        return ans;
+        vector<int>a;
+        return a;
     }
 };
