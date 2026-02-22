5b61653ae3cee9db2a76f403a9a4ee92cb1abca9
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-02-22 22:12:24 +0530

    Time: 2 ms (65.98%), Space: 9.4 MB (56.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,77 +1,68 @@
 class Solution {
 public:
 
-    bool r(int i,int j,string a,string b,string c,vector<vector<int>>&dp){
-        if ((i+j)>=c.size()){
-            return true;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        bool temp=false;
-        if(i<a.size() && c[i+j]==a[i]){
-            temp=r(i+1,j,a,b,c,dp);
-        }
-        if(j<b.size() && c[i+j]==b[j]){
-            temp=temp|r(i,j+1,a,b,c,dp);
-        }
-        dp[i][j]=temp;
-        return temp;
-    }
+    // bool r(int i,int j,string a,string b,string c,vector<vector<int>>&dp){
+    //     if ((i+j)>=c.size()){
+    //         return true;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     bool temp=false;
+    //     if(i<a.size() && c[i+j]==a[i]){
+    //         temp=r(i+1,j,a,b,c,dp);
+    //     }
+    //     if(j<b.size() && c[i+j]==b[j]){
+    //         temp=temp|r(i,j+1,a,b,c,dp);
+    //     }
+    //     dp[i][j]=temp;
+    //     return temp;
+    // }
 
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size()!=s3.size()){
             return false;
         }
-        vector<vector<int>>dp1(s1.size()+1,vector<int>(s2.size()+1,-1));
-        return r(0,0,s1,s2,s3,dp1);
+        // vector<vector<int>>dp1(s1.size()+1,vector<int>(s2.size()+1,-1));
+        // return r(0,0,s1,s2,s3,dp1);
          
         if(s1.size()==0 || s2.size()==0){
-            return true;
+            if(s1.size()==0){
+                if(s2==s3){
+                    return true;
+                }
+            }
+            else if(s1==s3){
+                return true;
+            }
+            return false;
         }
 
         vector<vector<bool>>dp(s1.size()+1,vector<bool>(s2.size()+1));
-        if(s1[s1.size()-1]==s3[s3.size()-1] && s2[s2.size()-1]==s3[s3.size()-1]){
-            dp[s1.size()][s2.size()-1]=true;
-            dp[s1.size()-1][s2.size()]=true;
-            dp[s1.size()][s2.size()]=true;
-
-        }
-        else if(s1[s1.size()-1]==s3[s3.size()-1]){
-            dp[s1.size()][s2.size()-1]=true;
-        }
-        else if(s2[s2.size()-1]==s3[s3.size()-1]){
-            dp[s1.size()-1][s2.size()]=true;
-        }
-        else{
-            return false;
-        }
+        dp[s1.size()][s2.size()]=true;
         for(int i=s1.size();i>=0;i--){
             for(int j=s2.size();j>=0;j--){
                 if(i<s1.size() && s1[i]==s3[i+j]){
                     dp[i][j]=dp[i][j]|dp[i+1][j];
-                }
-                if(j==(s2.size()-1)){
-                    cout<<s2[j]<<" "<<s3[i+j]<<" "<<i<<endl;
                 }
                 if(j<s2.size() && s2[j]==s3[i+j]){
                     dp[i][j]=dp[i][j]|dp[i][j+1];
                 }
             }
         }
-        for(auto i:dp){
-            for(bool j:i){
-                cout<<j<<" ";
-            }
-            cout<<endl;
-        }
-        cout<<endl;
-        for(auto i:dp1){
-            for(bool j:i){
-                cout<<j<<" ";
-            }
-            cout<<endl;
-        }
+        // for(auto i:dp){
+        //     for(bool j:i){
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // cout<<endl;
+        // for(auto i:dp1){
+        //     for(bool j:i){
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         return dp[0][0];
         
     }
