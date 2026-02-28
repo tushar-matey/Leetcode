e626d48e104b8d3828b5621083d6d42c2a41ec92
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-02-28 18:27:25 +0530

    Time: 46 ms (5.34%), Space: 112 MB (7.84%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,12 +5,11 @@ public:
         st.push({t[0],0});
         vector<int>ans(t.size());
         for(int i=1;i<t.size();i++){
-            if(t[i]>st.top().first){
-                while(!st.empty() && t[i]>st.top().first){
-                    ans[st.top().second]=i-st.top().second;
-                    st.pop();
-                }
+            while(!st.empty() && t[i]>st.top().first){
+                ans[st.top().second]=i-st.top().second;
+                st.pop();
             }
+            
             st.push({t[i],i});
         }
         return ans; 
