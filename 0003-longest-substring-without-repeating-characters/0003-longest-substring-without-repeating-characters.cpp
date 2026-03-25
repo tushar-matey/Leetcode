d4171bbba088cc0c177ee3d5808382137dc3f6de
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-03-25 07:24:05 +0530

    Time: 4 ms (72.64%), Space: 11.6 MB (75.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -10,16 +10,14 @@ public:
         int ans=1;
         while(j<s.size()){
             if(m.find(s[j])==m.end() || m[s[j]]<i){
-                ans=max(ans,j-i+1);
                 m[s[j]]=j;
-                j++;
             }
             else{
                 i=m[s[j]]+1;
-                ans=max(ans,j-i+1);
                 m[s[j]]=j;
-                j++;
             }
+            ans=max(ans,j-i+1);
+            j++;
         }
         return ans;
 
