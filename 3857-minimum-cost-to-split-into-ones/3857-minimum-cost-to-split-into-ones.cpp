70658f3accb2f0096453aee361f8c624d7c1a6a4
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-08-06 01:23:24 +0530

    Time: 0 ms (100%), Space: 8.1 MB (63%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,30 +1,9 @@
 class Solution {
 public:
 
-    int r(int x,vector<int>dp){
-        if(x==2){
-            return 1;
-        }
-        if(x==1){
-            return 0;
-        }
-        if(dp[x]!=-1){
-            return dp[x];
-        }
-        int ans=INT_MAX;
-        for(int i=1;i<x;i++){
-            ans=min(ans,i*(x-i)+r(i,dp)+r(x-i,dp));
-        }
-        dp[x]=ans;
-        return ans;
-    }
     int minCost(int n) {
-        vector<int>dp(n+1,-1);
-        // int ans=r(n,dp);
-        int ans=0;
-        for(int i=1;i<n;i++){
-            ans+=i;
-        }
+        n--;
+        int ans=(n*(n+1))/2;
         return ans;
     }
 };
