9f7109f03d6b87a55f6c24ba5b5ed74d87a6df6d
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-09-01 06:29:58 +0530

    Time: 71 ms (5.05%), Space: 17.9 MB (19.3%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+
+    // int r(map<int,int>::iterator it,map<int,int>&m,unordered_map<int,int>&dp){
+    //     if(it==m.end()){
+    //         return 0;
+    //     }
+    //     if(dp.find(it->first)!=dp.end()){
+    //         return dp[it->first];
+    //     }
+    //     int val=it->second*it->first;
+    //     int k=it->first;
+    //     //notTake
+    //     it++;
+    //     int notTake=r(it,m,dp);
+    //     //take
+    //     int take=INT_MIN;
+    //     if(it!=m.end()){
+    //         if(k+1==it->first){
+    //             it++;
+    //         }
+    //         take=r(it,m,dp)+val;
+    //     }
+    //     else{
+    //         take=val;
+    //     }
+    //     dp[it->first]=max(take,notTake);
+    //     return max(take,notTake);
+    // }
+    int deleteAndEarn(vector<int>& nums) {
+        map<int,int>m;
+        unordered_map<int,int>dp;
+        for(int i:nums){
+            if(m.find(i)==m.end()){
+                m[i]=1;
+            }
+            else{
+                m[i]++;
+            }
+        }
+
+        auto it=m.begin();
+        dp[it->first]=it->second*it->first;
+        auto prev1=it;
+        it++;
+        if(it==m.end()){
+            return dp[prev1->first];
+        }
+        if(prev1->first+1==it->first){
+            dp[it->first]=max(dp[prev1->first],it->second*it->first);
+        }
+        else{
+            dp[it->first]=it->second*it->first+dp[prev1->first];
+        }
+        auto prev=it;
+        it++;
+        while(it!=m.end()){
+            int val=it->second*it->first;
+            //notTake
+            int notTake=dp[prev->first];
+            // if(it->first==4){
+            //     cout<<notTake<<" "<<dp[prev->first]<<" "<<prev->first<<endl;
+            // }
+            //take
+            int take=INT_MIN;
+            
+            if(prev->first+1==it->first){
+                take=dp[prev1->first]+val;
+            }
+            else{
+                take=dp[prev->first]+val;
+            }
+            dp[it->first]=max(notTake,take);
+            prev1=prev;
+            prev=it;
+            it++;
+
+        }
+        for(auto i:dp){
+            cout<<i.first<<" "<<i.second<<endl;
+        }
+        return dp[prev->first];
+    }
+};
