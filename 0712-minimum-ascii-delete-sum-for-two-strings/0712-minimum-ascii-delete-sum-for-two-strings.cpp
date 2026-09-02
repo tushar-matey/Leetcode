85e4abcac8bfec87d3fa09d61ae1ac98ddd588ba
Tushar Sharad Matey <167842404+tushar-matey@users.noreply.github.com>
2026-09-02 11:17:26 +0530

    Time: 9 ms (93.28%), Space: 9.5 MB (98.4%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-
-    int r(int i,int j,const string &a,const string &b,vector<vector<int>>dp){
-        if(i>=a.size() || j>=b.size()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(a[i]==b[j]){
-            return a[i]+b[j]+r(i+1,j+1,a,b,dp);
-        }
-        return dp[i][j]=max(r(i+1,j,a,b,dp),r(i,j+1,a,b,dp));
-    }
-
     int minimumDeleteSum(string a, string b) {
-        vector<vector<int>>dp(a.size()+1,vector<int>(b.size()+1));
-        // vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
-        // int ans= r(0,0,s1,s2,dp);
+        vector<int>curr(b.size()+1);
+        vector<int>prev(b.size()+1);
         for(int i=a.size()-1;i>=0;i--){
             for(int j=b.size()-1;j>=0;j--){
                 if(a[i]==b[j]){
-                    dp[i][j]=a[i]+b[j]+dp[i+1][j+1];
+                    curr[j]=a[i]+b[j]+prev[j+1];
                 }
                 else{
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                    curr[j]=max(prev[j],curr[j+1]);
                 }
             }
+            prev=curr;
         }
-        // for(auto i:dp){
-        //     for(int j:i){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         int a1=0;
         for(char i:a){
             a1+=i;
@@ -41,6 +21,6 @@ public:
         for(char i:b){
             a1+=i;
         }
-        return a1-dp[0][0];
+        return a1-curr[0];
     }
 };
